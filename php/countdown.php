72145b19c1f09b3405e641a9bc8f7563fe1ce690
makimb2 <matthewakimball@gmail.com>
2015-01-26 23:46:22 -0700

    countdown assignments
    this has countdown.php & index.html for the countdown js

--- php/countdown.php
+++ php/countdown.php
@@ -1,14 +1,21 @@
 <?php
 
-if (PHP_SAPI !== 'cli') {
-    die (PHP_EOL.'Please run '.__FILE__.' from the command-line.'.PHP_EOL.PHP_EOL);
-}
+//if (PHP_SAPI !== 'cli') {
+  //  die (PHP_EOL.'Please run '.__FILE__.' from the command-line.'.PHP_EOL.PHP_EOL);
+//}
 
 function countdown($seconds)
 {
-    // ...
+    $x = $seconds;
+    while ($x >= 0) {
+    	echo $x;
+    	$x--;
+    	
+    }
+    
+
 }
 
-countdown(5);
+countdown(10);
 
-die('0 All done...');
+//die('0 All done...');
